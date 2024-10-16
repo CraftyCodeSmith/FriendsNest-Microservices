fa6caad1ad241cc673af54f7477810e74071f559
Tushar Rathi <tushar.rathi.tech@gmail.com>
2024-10-16 10:44:21 +0530

    Streaming: Added java service for rtc-streaming

---
+++ rtc/src/test/java/com/CraftCodeSmith/rtc/FriendsNestRtcStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CraftCodeSmith.rtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsNestRtcStreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
